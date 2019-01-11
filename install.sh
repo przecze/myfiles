519073416ef93012c0b7cea3c5ef069e4118c606
Przemyslaw Czechowski <p.czechowsk2@samsung.com>
2019-01-11 16:05:51 +0100

    Interactive labeling in install script

--- install.sh
+++ install.sh
@@ -4,5 +4,9 @@ VIMRC=$HOME/.vimrc
 BASHRC=$HOME/.bashrc
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+echo "Label this machine:"
+read label
+label="["$label"]"
+echo "HOSTNAME_LABEL=$label" >> $BASHRC
 echo "source $DIR/vimrc.ext" >> $VIMRC
 echo "source $DIR/bashrc.ext" >> $BASHRC
